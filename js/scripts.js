01d9b6ecc541b5312028f9c4ed7bbf39764f3f45
Manuel Raygoza <manny.raygoza@gmail.com>
2022-09-10 11:58:50 -0700

    WIP: added branching fornumStringArray.

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,11 @@
 
 const numStringArray = [0, 'beep', 'boop', 3, 4, 5];
 numStringArray.forEach(function(numberBeep) {
-  if (numStringArray.toString(1) === 'beep');
-  return 'beep';
+  if (numStringArray.toString(1) === 'beep') {
+    return 'beep';
+  } else if (numStringArray.toString(2) === 'boop')
+    return 'boop';
+  
 });
 
 
