10b4ba693944936908c171122bc3227cd6560743
Manuel Raygoza <manny.raygoza@gmail.com>
2022-09-10 12:23:56 -0700

    WIP: Test passed for user input number 3 now says wont you be my neighbor.

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,15 @@
 
 //Business Logic:
 
-const numStringArray = [0, 'beep', 'boop', 3, 4, 5];
+const numStringArray = [0, 'beep', 'boop', "won't you be my neightbor?", 4, 5];
 numStringArray.forEach(function(numberBeep) {
   if (numStringArray.toString(1) === 'beep') {
     return 'beep';
-  } else if (numStringArray.toString(2) === 'boop')
+  } else if (numStringArray.toString(2) === 'boop') {
     return 'boop';
-  
+  } else if (numStringArray.toString(3) === "won't you be my neightbor?") {
+    return "won't you be my neightbor?";
+  }
 });
 
 
